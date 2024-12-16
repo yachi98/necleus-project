a0eb09341cf5b5a299af3c1d60083a293bc4b597
Daniel Yachnikov Hughes <daniel.lloyd.hughes@gmail.com>
2024-12-16 00:37:35 +0000

    removed not needed code to reduce duplication

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
-// const accordionButtons = document.querySelectorAll(".accordion-title");
+const accordionButtons = document.querySelectorAll(".accordion-title");
 
-// accordionButtons.forEach((button) => {
-//   button.addEventListener("click", function () {
-//     const content = this.nextElementSibling;
+accordionButtons.forEach((button) => {
+  button.addEventListener("click", function () {
+    const content = this.nextElementSibling;
 
-//     if (content.style.display === "block") {
-//       content.style.display = "none";
-//     } else {
-//       content.style.display = "block";
-//     }
-//   });
-// });
-
-document.addEventListener("click", (e) => {
-  if (e.target.classList.contains("accordion-title")) {
-    const content = e.target.nextElementSibling;
-
-    // Toggle the display property
-    content.style.display =
-      content.style.display === "block" ? "none" : "block";
-  }
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
 });
+
+// document.addEventListener("click", (e) => {
+//   if (e.target.classList.contains("accordion-title")) {
+//     const content = e.target.nextElementSibling;
+
+//     // Toggle the display property
+//     content.style.display =
+//       content.style.display === "block" ? "none" : "block";
+//   }
+// });
 
 const menuBtn = document.querySelector(".mobile-menu-btn");
 const mobileNavMenu = document.querySelector(".mobile-nav-menu");
