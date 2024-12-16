8b8998263c43115cc021de729ac6b5c29aab3bf6
Daniel Yachnikov Hughes <daniel.lloyd.hughes@gmail.com>
2024-12-16 19:42:48 +0000

    hamburger menu works successfully and fixed accordion bug

--- main.js
+++ main.js
@@ -1,31 +1,36 @@
-const accordionButtons = document.querySelectorAll(".accordion-title");
+const accordionButtons = document.querySelectorAll(".accordion-btn");
+const accordions = document.querySelectorAll(".accordion-content");
+const accordionButtonIds = [
+  "accordion-btn-1",
+  "accordion-btn-2",
+  "accordion-btn-3",
+];
 
 accordionButtons.forEach((button) => {
   button.addEventListener("click", function () {
-    const content = this.nextElementSibling;
+    const accordionButtonId = accordionButtonIds.find((id) => id === button.id);
+    let accordion;
+    if (accordionButtonId === "accordion-btn-1") {
+      accordion = document.querySelector("#accordion1");
+    }
+    if (accordionButtonId === "accordion-btn-2") {
+      accordion = document.querySelector("#accordion2");
+    }
+    if (accordionButtonId === "accordion-btn-3") {
+      accordion = document.querySelector("#accordion3");
+    }
 
-    if (content.style.display === "block") {
-      content.style.display = "none";
+    if (accordion.style.display === "block") {
+      accordion.style.display = "none";
     } else {
-      content.style.display = "block";
+      accordion.style.display = "block";
     }
   });
 });
-
-// document.addEventListener("click", (e) => {
-//   if (e.target.classList.contains("accordion-title")) {
-//     const content = e.target.nextElementSibling;
-
-//     // Toggle the display property
-//     content.style.display =
-//       content.style.display === "block" ? "none" : "block";
-//   }
-// });
 
 const menuBtn = document.querySelector(".mobile-menu-btn");
 const mobileNavMenu = document.querySelector(".mobile-nav-menu");
 
 menuBtn.addEventListener("click", () => {
-  console.log(mobileNavMenu.classList);
-  mobileNavMenu.classList.toggle("open");
+  mobileNavMenu.classList.toggle("opened");
 });
